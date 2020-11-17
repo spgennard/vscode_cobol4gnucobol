9f5f27bef6a5d5e0b5e4ea5d7ea28acf11d5c7e0
Stephgen Gennard <stephen@gennard.net>
2020-11-17 19:29:34 +0000

    tweak

--- publishit2gitonly.sh
+++ publishit2gitonly.sh
@@ -1,8 +1,9 @@
 # exit on error
+set -x
 set -e
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 
-npx git-changelog-command-line --to-ref=refs/heads/main --ignore-commits-without-issue -std -tec "
+npx git-changelog-command-line --to-ref refs/heads/main --ignore-commits-without-issue -std -tec "
 # Changelog
 
 Changelog for {{ownerName}} {{repoName}}.
