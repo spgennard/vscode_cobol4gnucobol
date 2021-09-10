3289413de5013a05f178c5785a0259aadf5d72ac
Stephgen Gennard <stephen@gennard.net>
2021-09-11 00:23:22 +0100

    add flip code back in

--- src/extension.ts
+++ src/extension.ts
@@ -14,7 +14,58 @@ function logMessage(message: string, ...parameters: any[]): void {
 	}
 }
 
+const gnuCOBOLSpecific: string[] = [
+	">>SOURCE FORMAT",
+	">>SET SOURCEFORMAT",
+	"CBL_GC_FORK",
+	"CBL_GC_GETOPT",
+	"CBL_GC_HOSTED",
+	"CBL_GC_NANOSLEEP",
+	"CBL_GC_WAITPID",
+	"CBL_OC_GETOPT",
+	"CBL_OC_HOSTED",
+	"CBL_OC_NANOSLEEP",
+	"CBL_OC_CTK",
+	"GNUCOBOL", 				// constant
+	"OPENCOBOL",				// constant
+	"COB_SCREEN_EXCEPTIONS",
+	"COB_SCREEN_EXCEPTIONS",
+	"FUNCTION CONCATENATE",
+	"COBC"
+];
+
+function isSourceGnuCOBOL(doc: TextDocument): boolean {
+	// never want to stop the open... so ensure it always works
+	try {
+		const maxLines = doc.lineCount < 128 ? doc.lineCount : 128;
+
+		for (let lineCount = 0; lineCount < maxLines; lineCount++) {
+			const line = doc.lineAt(lineCount).text;
+			const lineUpper = line.toUpperCase();
+			for (const gnuItem of gnuCOBOLSpecific) {
+				if (lineUpper.indexOf(gnuItem) !== -1) {
+					return true;
+				}
+			}
+		}
+	} catch {
+		//
+	}
+
+	return false;
+}
+
 function flip_plaintext(doc: TextDocument) {
+
+	switch (doc.languageId) {
+		case 'COBOLIT':
+		case 'COBOL':
+			if (isSourceGnuCOBOL(doc)) {
+				vscode.languages.setTextDocumentLanguage(doc, "GnuCOBOL");
+				return;
+			}
+	}
+
 	if (doc.languageId === 'plaintext' || doc.languageId === 'tsql') {  // one tsql ext grabs .lst!
 		const lcount = doc.lineCount;
 		if (lcount >= 2) {
@@ -40,8 +91,8 @@ export function activate(context: vscode.ExtensionContext) {
 	}
 
 	const dumpfileSelector = [
-        { scheme: 'file', language: 'COBOL_GNU_DUMPFILE' }
-    ];
+		{ scheme: 'file', language: 'COBOL_GNU_DUMPFILE' }
+	];
 	const dumpfileSymbolProvider = new DumpFileSymbolProvider();
 	context.subscriptions.push(languages.registerDocumentSymbolProvider(dumpfileSelector, dumpfileSymbolProvider));
 	COBOLOutputChannel.clear();
