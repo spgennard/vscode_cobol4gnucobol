e8e450b9b4b1fd67e9e10b50a55c24e9f7aa77a6
Stephgen Gennard <stephen@gennard.net>
2021-09-17 22:23:53 +0100

    update

--- publishit.sh
+++ publishit.sh
@@ -2,40 +2,8 @@
 set -e
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 
-npx git-changelog-command-line --to-ref refs/heads/main --ignore-commits-without-issue -std -tec "
-# Changelog
+./gen_changelog.sh
 
-Changelog for {{ownerName}} {{repoName}}.
-
-{{#tags}}
-## {{name}}
- {{#issues}}
-  {{#hasIssue}}
-   {{#hasLink}}
-### {{name}} [{{issue}}]({{link}}) {{title}} {{#hasIssueType}} *{{issueType}}* {{/hasIssueType}} {{#hasLabels}} {{#labels}} *{{.}}* {{/labels}} {{/hasLabels}}
-   {{/hasLink}}
-   {{^hasLink}}
-### {{name}} {{issue}} {{title}} {{#hasIssueType}} *{{issueType}}* {{/hasIssueType}} {{#hasLabels}} {{#labels}} *{{.}}* {{/labels}} {{/hasLabels}}
-   {{/hasLink}}
-  {{/hasIssue}}
-  {{^hasIssue}}
-### {{name}}
-  {{/hasIssue}}
-
-  {{#commits}}
-**{{{messageTitle}}}**
-
-{{#messageBodyItems}}
- * {{.}}
-{{/messageBodyItems}}
-
-[{{hash}}](https://github.com/{{ownerName}}/{{repoName}}/commit/{{hash}}) *{{commitTime}}*
-
-  {{/commits}}
-
- {{/issues}}
-{{/tags}}
-" >CHANGELOG.md
 git commit -m "Update CHANGELOG.md" CHANGELOG.md && true
 git push
 git tag -f $PACKAGE_VERSION
