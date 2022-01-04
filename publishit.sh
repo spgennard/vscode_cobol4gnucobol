a7f639b4cd963080906fb29856c8f50f8e2c847f
Stephgen Gennard <stephen@gennard.net>
2022-01-04 20:23:30 +0000

    bump

--- publishit.sh
+++ publishit.sh
@@ -1,25 +1,22 @@
 # exit on error
 set -e
+
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+git tag -f $PACKAGE_VERSION
+git push --tags --force
 
 ./gen_changelog.sh
 git commit -m "Update CHANGELOG.md" CHANGELOG.md && true
 git push
-git tag -f $PACKAGE_VERSION
-git push --tags --force
+
 git clean -fdx
 npm install
 git push
+#npm run beforepublish
 vsce publish
+#npm run afterpublish
 rm -f *.vsix
-vsce package
-COMMIT_LOG=$(git log -1 --format='%ci %H %s')
-PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
-github-release upload \
-  --owner=spgennard \
-  --commit main \
-  --repo=vscode_cobol4gnucobol \
-  --tag="$PACKAGE_VERSION" \
-  --name=$PACKAGE_VERSION \
-  --body="${COMMIT_LOG}" \
-  gnucobol*.vsix
+
+npm-check-updates
+
+echo "use: ncu -u"
