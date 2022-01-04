62a5b1c93bcfa29354ddfa0414e8d16ae2d470b5
Stephgen Gennard <stephen@gennard.net>
2022-01-04 20:14:35 +0000

    update

---
+++ publishit_lite.sh
@@ -0,0 +1,23 @@
+# exit on error
+set -e
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+
+./gen_changelog.sh
+
+git commit -m "Update CHANGELOG.md" CHANGELOG.md && true
+git push
+git tag -f $PACKAGE_VERSION
+git push --tags --force
+rm -f *.vsix
+vsce publish
+vsce package
+COMMIT_LOG=$(git log -1 --format='%ci %H %s')
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+github-release upload \
+  --owner=spgennard \
+  --commit main \
+  --repo=vscode_cobol4gnucobol \
+  --tag="$PACKAGE_VERSION" \
+  --name=$PACKAGE_VERSION \
+  --body="${COMMIT_LOG}" \
+  gnucobol*.vsix
